213760517152ecb1f42d257a7a0eed82e1d3ab7c
Nadeen <75531472+NadeenXo@users.noreply.github.com>
2022-07-28 20:28:38 +0200

    order now

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*navbar*/ 
 let menu = document.querySelector('#menubar');
 let navbar = document.querySelector('.mynavbar');
 
@@ -6,4 +7,19 @@ menu.onclick = () => {
 }
 window.onscroll = () => {
     navbar.classList.remove('active');
+}
+
+/*email validation*/
+function submitEmail(){
+   /*  console.log(document.getElementById("InputEmail").value); */
+    let inp = document.getElementById("InputEmail");
+    if(inp.checkValidity()){
+        /* document.getElementById("nomail").innerHTML =inp.validationMessage; */
+        alert("great .. you have been one of us!");
+        inp.value="";
+    }else{
+        document.getElementById("nomail").innerHTML =inp.validationMessage;
+       
+    }
+    
 }
